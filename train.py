097b70e07bd4a142fafae23d627e82cd2c1d99f6
PSRahul <psrahul.ml@gmail.com>
2025-01-14 18:29:03 +0530

    added get_dataset and logger

--- train.py
+++ train.py
@@ -1,10 +1,35 @@
-import hydra
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import OmegaConf
+import logging
+
+# Basic configuration for the Logger
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
 
 
-@hydra.main(version_base=None, config_path="config", config_name="config")
-def main(cfg : DictConfig) -> None:
-    print(OmegaConf.to_yaml(cfg))
+def load_dataset_to_string(config: OmegaConf) -> str:
+    input_file = open(config.dataset.text_file_path)
+    file_content = input_file.read()
+    input_file.close()
+
+    return file_content
+
+
+def get_vocab(text_dataset: str) -> str:
+    vocab = sorted(list(set(text_dataset)))
+    return vocab
+
+
+def main():
+    config = OmegaConf.load("config/config.yaml")
+    logger.info("Read file from %s", config.dataset.text_file_path)
+    text_dataset = load_dataset_to_string(config)
+    logger.info("Read Success")
+    vocab = get_vocab(text_dataset)
+    logging.info(vocab)
+
 
 if __name__ == "__main__":
     main()
